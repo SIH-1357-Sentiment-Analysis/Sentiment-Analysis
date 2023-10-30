54c1ec2c0542a62974633eb42be96d0f0270c456
Viswaa Ramasubramanian <115810410+viswaaofficial@users.noreply.github.com>
2023-10-30 22:34:24 +0530

    Update Twitter Sentiment Analysis.py

--- Twitter_Analysis/Twitter Sentiment Analysis.py
+++ Twitter_Analysis/Twitter Sentiment Analysis.py
@@ -1,48 +1,36 @@
-import heapq
-goal_state = [[1, 2, 3], [8, 0, 4], [7, 6, 5]]
-initial_state = [[2, 8, 3], [1, 6, 4], [7, 0, 5]]
-def misplaced_tiles(state):
-    misplaced = 0
-    for i in range(3):
-        for j in range(3):
-            if state[i][j] != goal_state[i][j] and state[i][j] != 0:
-                misplaced += 1
-    return misplaced
-def find_moves(state):
-    moves = []
-    for i in range(3):
-        for j in range(3):
-            if state[i][j] == 0:
-                if i > 0:
-                    moves.append((i, j, i - 1, j))
-                if i < 2:
-                    moves.append((i, j, i + 1, j))
-                if j > 0:
-                    moves.append((i, j, i, j - 1))
-                if j < 2:
-                    moves.append((i, j, i, j + 1))
-    return moves
-def a_star(initial_state, heuristic):
-    open_list = [(heuristic(initial_state), 0, initial_state)]
-    closed_set = set()
-    while open_list:
-        _, g, current_state = heapq.heappop(open_list)
-        if current_state == goal_state:
-            return current_state
-        closed_set.add(tuple(map(tuple, current_state)))
-        for move in find_moves(current_state):
-            i, j, ni, nj = move
-            new_state = [list(row) for row in current_state]
-            new_state[i][j], new_state[ni][nj] = new_state[ni][nj], new_state[i][j]
-            
-            if tuple(map(tuple, new_state)) not in closed_set:
-                h = heuristic(new_state)
-                heapq.heappush(open_list, (g + h, g + 1, new_state))
-    return None
-result = a_star(initial_state, misplaced_tiles)
-if result:
-    print("Solution found:")
-    for row in result:
-        print(row)
-else:
-    print("No solution found.")
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from scipy.special import softmax
+import streamlit as st
+st.title('Twitter Tweets sentiment analysis')
+tweet = st.text_area('Please give a tweet from twitter')
+tweet_words = []
+for word in tweet.split(' '):
+    if word.startswith('@') and len(word) > 1:
+        word = '@user'
+    elif word.startswith('http'):
+        word = "http"
+    tweet_words.append(word)
+tweet_proc = " ".join(tweet_words)
+roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+model = AutoModelForSequenceClassification.from_pretrained(roberta)
+tokenizer = AutoTokenizer.from_pretrained(roberta)
+labels = ['Negative', 'Neutral', 'Positive']
+encoded_tweet = tokenizer(tweet_proc, return_tensors='pt')
+# output = model(encoded_tweet['input_ids'], encoded_tweet['attention_mask'])
+output = model(**encoded_tweet)
+scores = output[0][0].detach().numpy()
+scores = softmax(scores)
+
+
+
+
+if st.button('Enter'):
+    for i in range(len(scores)):
+        l = labels[i]
+        s = scores[i]
+        st.write(l)
+        st.write(s)
+
+
+# Run in Terminal
+# streamlit run filename.py
